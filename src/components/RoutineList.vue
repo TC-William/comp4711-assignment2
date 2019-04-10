dc3868e12e5b27bda73ecf0daf012a9985cf5c6f
keithliu03 <keithliu2013@gmail.com>
2019-04-10 13:59:36 -0700

    RoutineList form test
    Signed-off-by: keithliu03 <keithliu2013@gmail.com>

--- src/components/RoutineList.vue
+++ src/components/RoutineList.vue
@@ -1,38 +1,36 @@
 <template>
-
     <b-row>
         <b-col cols="12">
-        
-      <b-jumbotron>
-        <b-form @submit="onSubmit">
-          <b-form-group id="fieldsetHorizontal"
-                    horizontal
-                    :label-cols="4"
-                    breakpoint="md"
-                    label="Enter Title">
-            <b-form-input id="title" v-model.trim="board.title"></b-form-input>
-          </b-form-group>
-          <b-form-group id="fieldsetHorizontal"
-                    horizontal
-                    :label-cols="4"
-                    breakpoint="md"
-                    label="Enter Description">
-              <b-form-textarea id="description"
-                         v-model="board.description"
-                         placeholder="Enter something"
-                         :rows="2"
-                         :max-rows="6">{{board.description}}</b-form-textarea>
-          </b-form-group>
-          <b-form-group id="fieldsetHorizontal"
-                    horizontal
-                    :label-cols="4"
-                    breakpoint="md"
-                    label="Enter Author">
-            <b-form-input id="author" v-model.trim="board.author"></b-form-input>
-          </b-form-group>
-          <b-button type="submit" variant="primary">Save</b-button>
-        </b-form>
-      </b-jumbotron>
+        <b-jumbotron>
+            <b-form @submit="onSubmit">
+            <b-form-group id="fieldsetHorizontal"
+                        horizontal
+                        :label-cols="4"
+                        breakpoint="md"
+                        label="Enter Title">
+                <b-form-input id="title" v-model.trim="board.title"></b-form-input>
+            </b-form-group>
+            <b-form-group id="fieldsetHorizontal"
+                        horizontal
+                        :label-cols="4"
+                        breakpoint="md"
+                        label="Enter Description">
+                <b-form-textarea id="description"
+                            v-model="board.description"
+                            placeholder="Enter something"
+                            :rows="2"
+                            :max-rows="6">{{board.description}}</b-form-textarea>
+            </b-form-group>
+            <b-form-group id="fieldsetHorizontal"
+                        horizontal
+                        :label-cols="4"
+                        breakpoint="md"
+                        label="Enter Author">
+                <b-form-input id="author" v-model.trim="board.author"></b-form-input>
+            </b-form-group>
+            <b-button type="submit" variant="primary">Save</b-button>
+            </b-form>
+        </b-jumbotron>
 	  
 	    </b-col>
   </b-row>
@@ -46,6 +44,14 @@
     import router from '../router'
 
     export default {
+        name: 'AddBoard',
+        data () {
+            return {
+            ref: firebase.firestore().collection('routines'),
+            board: {}
+            }
+        },
+
         name: 'RoutineList',
         data () {
             return {
@@ -58,6 +64,7 @@
                 ref: firebase.firestore().collection('routines'),
             }
         },
+        
         created () {
             this.ref.onSnapshot((querySnapshot) => {
                 this.routines = [];
@@ -73,6 +80,22 @@
             details (routine) {
                 router.push({ name: 'Showroutine', params: { id: routine.key }})
             }
+
+            onSubmit (evt) {
+                evt.preventDefault()
+
+                this.ref.add(this.routine).then((docRef) => {
+                    this.routine.title = ''
+                    this.routine.description = ''
+                    this.routine.author = ''
+                    router.push({
+                    name: 'AddBoard'
+                    })
+                })
+                .catch((error) => {
+                    alert("Error adding document: ", error);
+                });
+            }
         }
     }
 </script>
@@ -81,5 +104,12 @@
     .table {
         width: 96%;
         margin: 0 auto;
+    }
+    .jumbotron {
+        padding: 2rem;
+    }
+    .edit-btn {
+        margin-right: 20px;
+        width: 70px;
     }
 </style>
